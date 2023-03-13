fc20d7444397142454f6f7bfe88cd7f95722d3d0
Miguel Ángel Medina Ramírez <migueliberto86@gmail.com>
2023-03-13 19:44:14 +0000

    Login and register added

---
+++ client/src/Login.jsx
@@ -0,0 +1,22 @@
+function Login() {  
+    return (
+      <div id="form" className="bg-bg-img bg-center bg-cover p-4 w-full h-screen flex flex-col items-center justify-center">
+        <form action="#" method="post" className="bg-transparent backdrop-blur-lg border-2 rounded-md flex justify-center items-start flex-wrap h-2/5 text-white tablet:text-xl laptop:w-3/5 laptop:h-3/5 desktop:text-2xl desktop:h-2/5 desktop:w-2/5">
+            <div className="w-full flex justify-between p-4 tablet:justify-evenly">
+              <label htmlFor="Email" className="tablet:w-1/4 tablet:p-2">Email</label>
+              <input type="email" name="Email" id="Email" className="border-b-2 border-current pl-2 tablet:w-2/4 tablet:p-2 bg-transparent rounded-sm"/>
+            </div>
+            <div className="w-full flex justify-between p-4 tablet:justify-evenly">
+              <label htmlFor="password" className="tablet:w-1/4 tablet:p-2">Password</label>
+              <input type="password" name="password" id="password" className="border-b-2 border-current pl-2	tablet:w-2/4 tablet:p-2 bg-transparent rounded-sm"/>
+            </div>
+            <div className="w-full flex justify-center p-4">
+              <button type="submit" className="border-2 border-current rounded-md p-2 hover:bg-white hover:text-violet-600 hover:scale-110 transition-all	shadow-low-box-shadow hover:shadow-high-box-shadow">Login</button>
+            </div>
+        </form>
+      </div>
+    )
+  }
+  
+export default Login
+  
--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -5,7 +5,21 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'bg-img': "url('img/background.jpg')",
+      }
+    },
+    screens: {
+      'mobile': '300px',
+      'tablet': '600px',
+      'laptop': '1000px',
+      'desktop': '1600px',
+    },
+    boxShadow: {
+      'low-box-shadow': '0 10px 20px rgba(0, 0, 0, 0.3)',
+      'high-box-shadow': '0 10px 20px rgba(0, 0, 0, 0.6)'
+    }
   },
   plugins: [],
 }
