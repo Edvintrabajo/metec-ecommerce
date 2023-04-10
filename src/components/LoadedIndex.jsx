80b802e95759b0c402af35cbb4c14ea000584313
Miguel Ángel Medina Ramírez <migueldevinfo@gmail.com>
2023-04-10 21:08:07 +0200

    add product card update

--- src/components/LoadedIndex.jsx
+++ src/components/LoadedIndex.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar";
 import Filters from "../components/Filters";
 import MainContent from "../components/MainContent";
+import AddProduct from "../components/admin/AddProduct";
 import { useContext } from 'react';
 import { Context } from '../context/Context';
 
@@ -11,8 +12,8 @@ const LoadedIndex = () => {
     <div id="loaded-index" className="hidden w-full h-full min-h-screen	">
         <Navbar />
         <Filters />
+        <AddProduct/>
         <MainContent />
-        {/* <AddProduct/> // Componente ADMIN*/}
     </div>
   );
 }
