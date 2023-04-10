473c837f53260a879fc062dc725260db166d4d9f
Edvin <EdvinTrabajo@gmail.com>
2023-04-10 21:46:50 +0100

    Añadido IndexAdmin y AdminCards)

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -4,11 +4,16 @@ import Login from '../components/Login'
 import Error404 from '../components/Error404'
 import Index from '../components/Index'
 import FiltredProducts from '../components/FiltredProducts'
+import IndexAdmin from '../components/admin/IndexAdmin'
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <Index />,
+    },
+    {
+      path: "/admin",
+      element: <IndexAdmin />,
     },
     {
       path: "/login",
