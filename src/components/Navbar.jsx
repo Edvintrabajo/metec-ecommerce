c9e3c40344671ff261a352d8dc8824a11a6a4906
Miguel Ángel Medina Ramírez <migueldevinfo@gmail.com>
2023-04-11 11:32:48 +0200

    Logo Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,9 @@ const Navbar = () => {
       id="navbar"
       className="w-full h-1/6 flex items-center bg-white p-4 text-4xl select-none z-20 relative"
     >
-      <a href="/" className="ml-4 flex justify-center items-center hover:scale-110 transition-all">
-        <img src={`./src/img/title-logo.png`} className="h-10 mr-4" alt="" />
-        <img src={`./src/img/logo-metec.png`} className="h-10" alt="" />
+      <a href="/" id="navbar-body" className="ml-4 flex justify-center items-center hover:scale-110 transition-all">
+        <img src={`./src/img/title-logo.png`} id="navbar-text" className="h-10 mr-4" alt="" />
+        <img src={`./src/img/logo-metec.png`} id="navbar-logo" className="h-10" alt="" />
       </a>
     </nav>
   );
