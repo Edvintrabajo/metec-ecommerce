7e4da699de925a38b5ca1ed2b332613dc90fba63
Miguel Ángel Medina Ramírez <migueldevinfo@gmail.com>
2023-04-19 08:16:58 +0200

    Style Update (Pending more)

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -22,33 +22,39 @@ function Product({product, displayForm, updateStates}) {
    } = useContext(Context);
   
   return (
-    // <div key={product.id} className='product-card flex flex-col justify-center items-center'>
-    //     <h2 className='text-2xl font-bold'>
-    //       {product.name}
-    //     </h2>
-    //     {/* <p>{product.brand}</p> */}
-    //     <img src={product.url} alt={product.image} className='m-4 w-40 h-40'/>
-        
-    //     <div className='w-full flex justify-evenly'>
-          
-          
-    //     </div>
-    // </div>
+        // ADMIN CARD
         <div key={product.id} className="flip-card">
         <div className="flip-card-inner">
           <div className="flip-card-front">
-            <img src={product.url} alt={product.name} />
-            <h2 className=' font-bold text-xl'>{product.name}</h2>
+            <div className='h-4/5 flex justify-center items-center'>
+              <img src={product.url} alt={product.name} />
+            </div>
+            <div className='bg-blue-gray-50 rounded-b-xl w-full h-1/5 flex flex-col justify-center'>
+              <h2 className=' font-bold text-xl'>{product.name}</h2>
+              <p>{product.description}</p>
+            </div>
           </div>
           <div className="flip-card-back">
-            <div className='flex flex-col justify-center h-3/4'>
-              <p>Marca: {product.brand}</p>
-              <p>Descripcion: {product.description}</p>
-              <p>Precio: {product.price}</p>
-              <p>Stock: {product.stock}</p>
+            <div className='h-3/4'>
+              
+              <div className='h-1/4 flex justify-center items-center border-b-2 bg-blue-gray-50 rounded-t-xl'>
+                <h3 className='font-bold text-xl'>{product.brand}</h3>
+              </div>
+              
+              <div className='h-3/4 flex justify-center'>
+                <div className='overflow-y-auto p-4'>
+                  <h4 className='font-bold text-lg m-4'
+                  >Details</h4>
+                  <p>Price: {product.price},00 €</p>
+                  <p>Stock: {product.stock}</p>
+                  <p>Ratings: {product.ratings}/10</p>
+                </div>
+
+              </div> 
+              
             </div>
             
-            <div className='flex justify-evenly items-center w-full h-1/4 border-t-2'>
+            <div className='flex justify-evenly items-center w-full h-1/4 border-t-2 bg-blue-gray-50 rounded-b-xl'>
               <button 
                 className='bg-danger text-white p-2 w-1/3 flex justify-between items-center hover:border-danger hover:bg-white hover:text-danger transition-all duration-300 ease-in-out'
                 onClick={() => {
