f876608550f5d359e26017f1f2343b2ff58c88f9
Miguel Ángel Medina Ramírez <migueldevinfo@gmail.com>
2023-04-21 17:36:56 +0200

    Style update (Button style fix)

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
 import { IoAddCircleOutline } from 'react-icons/io5'
 import { displayForm } from "../controllers/products/products.functions";
+import { Button } from "@material-tailwind/react";
 function AddButton() {
   return (
-    <button 
-      className='bg-info hover:border-info hover:bg-white hover:text-info absolute top-28 right-10 flex justify-between items-center p-2 w-24 transition-all duration-300 ease-in-out'
+    // <button 
+    //   className='bg-info hover:border-info hover:bg-white hover:text-info absolute top-28 right-10 flex justify-between items-center p-2 w-24 transition-all duration-300 ease-in-out'
+    //   onClick={() => {
+    //     displayForm('create-product-form')
+    //   }}
+    // >
+    //     <p>Add</p>
+    //     <IoAddCircleOutline />
+    // </button>
+    <Button
+      className='bg-info hover:border-info hover:bg-white hover:text-info absolute top-28 right-10 flex justify-between items-center p-3 w-20 transition-all duration-300 ease-in-out'
       onClick={() => {
         displayForm('create-product-form')
       }}
     >
-        <p>Add</p>
-        <IoAddCircleOutline className='text-white ' />
-    </button>
+      <p>Add</p>
+      <IoAddCircleOutline className='scale-150'/>
+    </Button>
   )
 }
 
