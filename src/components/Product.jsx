a75f89aeb8dd8d330e962215e0be74b902eb7c45
Miguel Ángel Medina Ramírez <migueldevinfo@gmail.com>
2023-04-21 17:26:14 +0200

    Card css removed (Completed card view)

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,78 +3,90 @@ import { useContext } from "react";
 import { Context } from "../context/Context";
 import { BsFillTrashFill } from 'react-icons/bs';
 import { MdModeEdit } from 'react-icons/md';
+import { MdOutlineShoppingCart } from 'react-icons/md';
 import '../Cards.css';
 
 function Product({product, displayForm, getStates}) {
   const { setProductIdEdit, setImageRefName, data, setData } = useContext(Context)
 
   return (
-        // ADMIN CARD
-        <div key={product.id} className="flip-card">
-        <div className="flip-card-inner">
-          
-          <div className="flip-card-front">
-            
-            <div className='h-4/5 flex justify-center items-center'>
-              <img src={product.url} alt={product.name} />
-            </div>
-            
-            <div className='bg-blue-gray-50 rounded-b-xl w-full h-1/5 flex flex-col justify-center'>
-              <h2 className=' font-bold text-xl'>{product.name}</h2>
-              <p>{product.description}</p>
-            </div>
-            
-          </div>
-          
-          <div className="flip-card-back">
-            <div className='h-3/4'>
-              
-              <div className='h-1/4 flex justify-center items-center border-b-2 bg-blue-gray-50 rounded-t-xl'>
-                <h3 className='font-bold text-xl'>{product.brand}</h3>
-              </div>
-              
-              <div className='h-3/4 flex justify-center'>
-                <div className='overflow-y-auto p-4'>
-                  <h4 className='font-bold text-lg m-4'
-                  >Details</h4>
-                  <p>Price: {product.price},00 €</p>
-                  <p>Stock: {product.stock}</p>
-                  <p>Ratings: {product.ratings}/10</p>
-                </div>
-              </div> 
-              
-            </div>
-            
-            <div className='flex justify-evenly items-center w-full h-1/4 border-t-2 bg-blue-gray-50 rounded-b-xl'>
-              
-              <button 
-                className='bg-danger text-white p-2 w-1/3 flex justify-between items-center hover:border-danger hover:bg-white hover:text-danger transition-all duration-300 ease-in-out'
-                onClick={() => {
-                  displayForm('delete-product-form')
-                  setProductIdEdit(product.id)
-                  getStates(product.id, data, setData)
-                }}>
-                Delete
-                <BsFillTrashFill />      
-              </button>
-              
-              <button 
-                className='bg-edit text-white p-2 w-1/3 flex justify-between items-center hover:border-edit hover:bg-white hover:text-edit transition-all duration-300 ease-in-out'
-                onClick={() => {
-                  displayForm('edit-product-form')
-                  setProductIdEdit(product.id)
-                  setImageRefName(product.imageRefName)
-                  getStates(product.id, data, setData)
-                }}>
-                Edit
-                <MdModeEdit />
-              </button>
+      // NEW-CARD
 
-            </div>
-          
+      <div className="rounded-xl w-60 h-80 p-4 bg-white relative overflow-visible text-black">
+        <div className="bg-white p-2 h-1/2 w-full rounded-lg transition-all shadow-low-card flex justify-center align-middle hover:shadow-high-card hover:-translate-y-(25%)">
+          <img className=' rounded-2xl w-2/3' src={product.url} alt={product.name} />
+        </div>
+        <div className="p-2 pb-6">
+          <h3 className="text-title font-black text-lg">{product.name}</h3>
+          <p className="text-sm">{product.description}</p>
+          <p>Ratings: {product.ratings}/10</p>
+            
+
+        </div>
+        <div className="w-full flex justify-center items-center pt-3 border-t-2">
+          <div className='w-1/2 text-left'>
+            <span className="text-title font-black text-lg">{product.price},00 €</span>
           </div>
+          
+          <div className='flex justify-end  w-1/2'>
+            
+            {/* USER-VIEW */}
+            {/* <button 
+              className="border-2 border-info flex p-2 rounded-full transition-all text-info hover:text-white hover:bg-info"
+              >
+              <MdOutlineShoppingCart />      
+            </button> */}
+
+            {/* ADMIN-VIEW */}
+            <button 
+              className="border-2 border-danger mr-2 flex p-2 rounded-full transition-all text-danger hover:text-white hover:bg-danger" 
+              onClick={() => {
+                displayForm('delete-product-form')
+                setProductIdEdit(product.id)
+                getStates(product.id, data, setData)
+              }}>
+              <BsFillTrashFill />      
+            </button>
+            
+            <button
+              className='border-2 border-edit flex p-2 rounded-full transition-all text-edit hover:text-white hover:bg-edit' 
+              onClick={() => {
+                displayForm('edit-product-form')
+                setProductIdEdit(product.id)
+                setImageRefName(product.imageRefName)
+                getStates(product.id, data, setData)
+              }}>
+              <MdModeEdit />
+            </button>
+          </div>
+
         </div>
       </div>
+      
+      //     {/* RATING */}
+
+      //     <div className="rating">
+      //       <label htmlFor="star-1">
+      //         <input className='hidden' type="radio" id="star-1" name="star-radio" value="star-1"/>
+      //         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path pathLength="360" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"></path></svg>
+      //       </label>
+      //       <label htmlFor="star-2">
+      //         <input className='hidden' type="radio" id="star-2" name="star-radio" value="star-1"/>
+      //         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path pathLength="360" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"></path></svg>
+      //       </label>
+      //       <label htmlFor="star-3">
+      //         <input className='hidden' type="radio" id="star-3" name="star-radio" value="star-1"/>
+      //         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path pathLength="360" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"></path></svg>
+      //       </label>
+      //       <label htmlFor="star-4">
+      //         <input className='hidden' type="radio" id="star-4" name="star-radio" value="star-1"/>
+      //         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path pathLength="360" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"></path></svg>
+      //       </label>
+      //       <label htmlFor="star-5">
+      //         <input className='hidden' type="radio" id="star-5" name="star-radio" value="star-1"/>
+      //         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path pathLength="360" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"></path></svg>
+      //       </label>
+      //     </div>
   )
 }
 
