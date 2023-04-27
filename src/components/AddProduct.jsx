6f13b5b7616be57a116daf1c4ab168e838f7bde8
Miguel Ángel Medina Ramírez <migueldevinfo@gmail.com>
2023-04-27 09:24:51 +0200

    Css succesfuly merged ( Pending new components )

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -1,122 +1,107 @@
-import React from 'react'
-import { useContext } from 'react'
-import { Context } from '../context/Context'
-import { displayForm, addProduct, addData, resetForm } from '../controllers/products/products.functions'
+import React from "react";
+import { useContext } from "react";
+import { Context } from "../context/Context";
+import {
+  displayForm,
+  addProduct,
+  addData,
+  resetForm,
+} from "../controllers/products/products.functions";
 import { Card, Typography, Input } from "@material-tailwind/react";
 
 function AddProduct() {
+  const { setProducts, data, setData } = useContext(Context);
 
-    const { setProducts, data, setData } = useContext(Context)
+  return (
+    <div
+      id="create-product-form"
+      className="absolute top-0 left-0 z-10 hidden h-full w-full items-center justify-center bg-b-rgba-4"
+    >
+      <Card className="w-2/3">
+        <form
+          id="create-product-container"
+          className="z-20 flex w-full flex-wrap items-center justify-evenly p-4"
+          onSubmit={(e) => {
+            e.preventDefault();
+            addData(data, setData);
+            addProduct(data, setData, setProducts);
+            displayForm("create-product-form");
+            resetForm("create-product-container");
+          }}
+        >
+          <Typography
+            className=" w-full p-4 text-info"
+            variant="h4"
+            color="blue-gray"
+          >
+            Add Product
+          </Typography>
+          <div className="mb-4 flex h-52 w-2/5 flex-wrap items-center justify-center">
+            <Input id="name" label="Name" type="text" required />
+            <Input id="brand" label="Brand" type="text" required />
+            <Input
+              id="price"
+              label="Price"
+              type="number"
+              required
+              min={0}
+              max={10000}
+            />
+            <Input
+              id="stock"
+              label="Stock"
+              type="number"
+              required
+              min={0}
+              max={10000}
+            />
+          </div>
+          <div className="mb-4 flex h-52 w-2/5 flex-wrap items-center justify-center">
+            <Input id="description" label="Description" type="text" required />
+            <Input
+              id="ratings"
+              label="Ratings"
+              type="number"
+              required
+              min={0}
+              max={10}
+            />
+            <Input id="category" label="Category" type="text" required />
+            <Input id="type" label="Type" type="text" required />
+          </div>
 
-    return (
-        <div 
-            id='create-product-form' 
-            className='absolute w-full h-full top-0 left-0 hidden justify-center items-center z-10'>
-            <Card
-                className='w-2/3'>
+          <div className=" w-4/6">
+            <input
+              id="image"
+              type="file"
+              required
+              accept="image/png, image/jpeg, image/jpg"
+            />
+          </div>
 
-                <form 
-                    id='create-product-container'
-                    className='w-full p-4 z-20 flex flex-wrap justify-evenly items-center'
-                    onSubmit={(e) => {
-                        e.preventDefault()
-                        addData(data, setData)
-                        addProduct(data, setData, setProducts)
-                        displayForm('create-product-form')
-                        resetForm('create-product-container')
-                    }}
-                    >
-                    <Typography className=' w-full p-4 text-info' variant="h4" color="blue-gray">
-                        Add Product
-                    </Typography>
-                    <div className='w-2/5 mb-4 h-52 flex flex-wrap justify-center items-center'>
-                        <Input
-                            id='name'
-                            label="Name"
-                            type="text"
-                            required
-                            />
-                        <Input
-                            id='brand'
-                            label='Brand' 
-                            type="text"
-                            required 
-                            />
-                        <Input
-                            id='price'
-                            label='Price'
-                            type="number"
-                            required 
-                            min={0} max={10000}
-                            />
-                        <Input
-                            id='stock'
-                            label='Stock'
-                            type="number"
-                            required
-                            min={0} max={10000}
-                            />
-                    </div>
-                    <div className='w-2/5 mb-4 h-52 flex flex-wrap justify-center items-center'>
-                        <Input
-                            id='description'
-                            label='Description'
-                            type="text"
-                            required 
-                            />
-                        <Input
-                            id='ratings'
-                            label='Ratings'
-                            type="number"
-                            required
-                            min={0} max={10}
-                            />
-                        <Input
-                            id='category'
-                            label='Category'
-                            type="text"
-                            required 
-                            />
-                        <Input
-                            id='type'
-                            label='Type'
-                            type="text"
-                            required 
-                            />
-                    </div>
+          <div className="mt-4 flex w-1/2 justify-evenly">
+            <button
+              className="w-20 rounded-md border-2 border-info bg-info p-1 text-white shadow-low-info transition-all hover:bg-white hover:text-info hover:shadow-high-info"
+              type="submit"
+            >
+              Add
+            </button>
 
-                    <div className=' w-4/6'>
-                        <input
-                            id='image'
-                            type="file"
-                            required
-                            accept="image/png, image/jpeg, image/jpg"
-                            />
-                    </div>
-
-                    <div className='flex justify-evenly w-1/2 mt-4'>
-                        <button 
-                            className='bg-info p-1 w-20 rounded-md text-white border-2 border-info hover:text-info hover:bg-white transition-all shadow-low-info hover:shadow-high-info'
-                            type='submit'>
-                            Add
-                        </button>
-
-                        <button
-                            className='bg-cancel p-1 w-20 rounded-md text-white border-2 border-cancel hover:text-cancel hover:bg-white transition-all shadow-low-cancel hover:shadow-high-cancel'
-                            type='button'
-                            onClick={() => {
-                                displayForm('create-product-form')
-                                resetForm('create-product-container')
-                                }}>
-                            Cancel
-                        </button>
-                    </div>
-
-                </form>
-            </Card>
-
-        </div>
-    )
+            <button
+              className="w-20 rounded-md border-2 border-cancel bg-cancel p-1 text-white shadow-low-cancel transition-all hover:bg-white hover:text-cancel hover:shadow-high-cancel"
+              type="button"
+              onClick={() => {
+                displayForm("create-product-form");
+                resetForm("create-product-container");
+              }}
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
+      </Card>
+    </div>
+  );
 }
 
-export default AddProduct
+export default AddProduct;
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,93 +1,97 @@
-import React from 'react'
+import React from "react";
 import { useContext } from "react";
 import { Context } from "../context/Context";
-import { BsFillTrashFill } from 'react-icons/bs';
-import { MdModeEdit } from 'react-icons/md';
-import { MdOutlineShoppingCart } from 'react-icons/md';
-import '../Cards.css';
+import { BsFillTrashFill } from "react-icons/bs";
+import { MdModeEdit } from "react-icons/md";
+import { MdOutlineShoppingCart } from "react-icons/md";
 
-function Product({product, displayForm, getStates}) {
-  const { setProductIdEdit, setImageRefName, data, setData } = useContext(Context)
+function Product({ product, displayForm, getStates }) {
+  const { setProductIdEdit, setImageRefName, data, setData } =
+    useContext(Context);
 
   return (
-      // NEW-CARD
+    // NEW-CARD
 
-      <div className="rounded-xl w-60 h-80 p-4 bg-white relative overflow-visible text-black">
-        <div className="bg-white p-2 h-1/2 w-full rounded-lg transition-all shadow-low-info flex justify-center align-middle hover:shadow-high-info hover:-translate-y-(25%)">
-          <img className=' rounded-2xl w-2/3' src={product.url} alt={product.name} />
+    <div className="relative h-80 w-60 overflow-visible rounded-xl bg-white p-4 text-black">
+      <div className="flex h-1/2 w-full justify-center rounded-lg bg-white p-2 align-middle shadow-low-info transition-all hover:-translate-y-(25%) hover:shadow-high-info">
+        <img
+          className=" w-2/3 rounded-2xl"
+          src={product.url}
+          alt={product.name}
+        />
+      </div>
+      <div className="p-2 pb-6">
+        <h3 className="text-title text-lg font-black">{product.name}</h3>
+        <p className="text-sm">{product.description}</p>
+        <p>Ratings: {product.ratings}/10</p>
+      </div>
+      <div className="flex w-full items-center justify-center border-t-2 pt-3">
+        <div className="w-1/2 text-left">
+          <span className="text-title text-lg font-black">
+            {product.price},00 €
+          </span>
         </div>
-        <div className="p-2 pb-6">
-          <h3 className="text-title font-black text-lg">{product.name}</h3>
-          <p className="text-sm">{product.description}</p>
-          <p>Ratings: {product.ratings}/10</p>
-            
 
-        </div>
-        <div className="w-full flex justify-center items-center pt-3 border-t-2">
-          <div className='w-1/2 text-left'>
-            <span className="text-title font-black text-lg">{product.price},00 €</span>
-          </div>
-          
-          <div className='flex justify-end  w-1/2'>
-            
-            {/* USER-VIEW */}
-            {/* <button 
+        <div className="flex w-1/2  justify-end">
+          {/* USER-VIEW */}
+          {/* <button 
               className="border-2  border-info shadow-low-info hover:shadow-high-info flex p-2 rounded-full transition-all text-info hover:text-white hover:bg-info"
               >
               <MdOutlineShoppingCart />      
             </button> */}
 
-            {/* ADMIN-VIEW */}
-            <button 
-              className="border-2 border-danger shadow-low-danger hover:shadow-high-danger mr-2 flex p-2 rounded-full transition-all text-danger hover:text-white hover:bg-danger" 
-              onClick={() => {
-                displayForm('delete-product-form')
-                setProductIdEdit(product.id)
-                getStates(product.id, data, setData)
-              }}>
-              <BsFillTrashFill />      
-            </button>
-            
-            <button
-              className='border-2 border-edit shadow-low-edit hover:shadow-high-edit flex p-2 rounded-full transition-all text-edit hover:text-white hover:bg-edit' 
-              onClick={() => {
-                displayForm('edit-product-form')
-                setProductIdEdit(product.id)
-                setImageRefName(product.imageRefName)
-                getStates(product.id, data, setData)
-              }}>
-              <MdModeEdit />
-            </button>
-          </div>
+          {/* ADMIN-VIEW */}
+          <button
+            className="mr-2 flex rounded-full border-2 border-danger p-2 text-danger shadow-low-danger transition-all hover:bg-danger hover:text-white hover:shadow-high-danger"
+            onClick={() => {
+              displayForm("delete-product-form");
+              setProductIdEdit(product.id);
+              getStates(product.id, data, setData);
+            }}
+          >
+            <BsFillTrashFill />
+          </button>
 
+          <button
+            className="flex rounded-full border-2 border-edit p-2 text-edit shadow-low-edit transition-all hover:bg-edit hover:text-white hover:shadow-high-edit"
+            onClick={() => {
+              displayForm("edit-product-form");
+              setProductIdEdit(product.id);
+              setImageRefName(product.imageRefName);
+              getStates(product.id, data, setData);
+            }}
+          >
+            <MdModeEdit />
+          </button>
         </div>
       </div>
-      
-      //     {/* RATING */}
+    </div>
 
-      //     <div className="rating">
-      //       <label htmlFor="star-1">
-      //         <input className='hidden' type="radio" id="star-1" name="star-radio" value="star-1"/>
-      //         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path pathLength="360" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"></path></svg>
-      //       </label>
-      //       <label htmlFor="star-2">
-      //         <input className='hidden' type="radio" id="star-2" name="star-radio" value="star-1"/>
-      //         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path pathLength="360" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"></path></svg>
-      //       </label>
-      //       <label htmlFor="star-3">
-      //         <input className='hidden' type="radio" id="star-3" name="star-radio" value="star-1"/>
-      //         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path pathLength="360" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"></path></svg>
-      //       </label>
-      //       <label htmlFor="star-4">
-      //         <input className='hidden' type="radio" id="star-4" name="star-radio" value="star-1"/>
-      //         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path pathLength="360" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"></path></svg>
-      //       </label>
-      //       <label htmlFor="star-5">
-      //         <input className='hidden' type="radio" id="star-5" name="star-radio" value="star-1"/>
-      //         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path pathLength="360" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"></path></svg>
-      //       </label>
-      //     </div>
-  )
+    //     {/* RATING */}
+
+    //     <div className="rating">
+    //       <label htmlFor="star-1">
+    //         <input className='hidden' type="radio" id="star-1" name="star-radio" value="star-1"/>
+    //         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path pathLength="360" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"></path></svg>
+    //       </label>
+    //       <label htmlFor="star-2">
+    //         <input className='hidden' type="radio" id="star-2" name="star-radio" value="star-1"/>
+    //         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path pathLength="360" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"></path></svg>
+    //       </label>
+    //       <label htmlFor="star-3">
+    //         <input className='hidden' type="radio" id="star-3" name="star-radio" value="star-1"/>
+    //         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path pathLength="360" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"></path></svg>
+    //       </label>
+    //       <label htmlFor="star-4">
+    //         <input className='hidden' type="radio" id="star-4" name="star-radio" value="star-1"/>
+    //         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path pathLength="360" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"></path></svg>
+    //       </label>
+    //       <label htmlFor="star-5">
+    //         <input className='hidden' type="radio" id="star-5" name="star-radio" value="star-1"/>
+    //         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path pathLength="360" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"></path></svg>
+    //       </label>
+    //     </div>
+  );
 }
 
-export default Product
+export default Product;
