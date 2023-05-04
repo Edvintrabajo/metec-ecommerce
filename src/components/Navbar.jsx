27f69ac3be460b54f257fd597d7f8542c87475e8
Miguel Ángel Medina Ramírez <migueldevinfo@gmail.com>
2023-05-04 11:33:55 +0200

    Profile & Shopingcart logos Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { getOrdersCount } from "../controllers/orders/orders.functions";
 import { useContext } from "react";
 import { Context } from "../context/Context";
 import { BiUser } from "react-icons/bi";
+import { MdOutlineShoppingCart } from "react-icons/md";
 function Navbar() {
 
   const { countOrders, totalPrice } = useContext(Context);
@@ -15,23 +16,10 @@ function Navbar() {
       </div>
       <div className="flex-none">
         <div className="dropdown dropdown-end">
-          <label tabIndex={0} className="btn btn-ghost btn-circle">
-            <div className="indicator">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-8 w-8"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                />
-              </svg>
-              <span className="badge badge-sm indicator-item">8</span>
+          <label tabIndex={0} className="btn btn-ghost btn-circle border-neutral-500 transition-all hover:border-transparent">
+            <div className="indicator rounded-full p-3">
+                <MdOutlineShoppingCart className="scale-150"/>
+              <span className="badge badge-sm indicator-item border-neutral-500">{ countOrders }</span>
             </div>
           </label>
           <div
@@ -49,9 +37,9 @@ function Navbar() {
           </div>
         </div>
         <div className="dropdown dropdown-end ml-5">
-          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-            <div className="w-10 rounded-full p-3 bg-blue-gray-700">
-              <BiUser className=" scale-150" />
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar border-neutral-500 transition-all hover:border-transparent">
+            <div className="w-10 rounded-full p-3">
+              <BiUser className=" scale-150 m-auto" />
             </div>
           </label>
           <ul
