728cf50cd27ccabb436cf659090c7dd9361a146f
Miguel Ángel Medina Ramírez <migueldevinfo@gmail.com>
2023-05-05 20:40:11 +0200

    Mobile media query updated

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -5,7 +5,7 @@ import { Button } from "@material-tailwind/react";
 function AddButton() {
   return (
     <button
-      className="absolute top-28 right-10 flex w-20 items-center justify-between rounded-lg bg-info p-2 shadow-low-info transition-all duration-300 ease-in-out hover:border-info hover:bg-white hover:text-info hover:shadow-high-info"
+      className="absolute top-36 right-3 flex w-20 items-center justify-between rounded-lg bg-info p-2 shadow-low-info transition-all duration-300 ease-in-out hover:border-info hover:bg-white hover:text-info hover:shadow-high-info tablet:right-5"
       onClick={() => {
         displayForm("create-product-form");
       }}
