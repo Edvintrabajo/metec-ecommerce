0c6660d4e3ed0ab8aae9796280860a07edcf47c1
Miguel Ángel Medina Ramírez <migueldevinfo@gmail.com>
2023-05-06 20:07:04 +0200

    Filter update

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -17,16 +17,17 @@ function Filters() {
         }}
       >
         <MenuHandler>
-          <label 
-              className="relative w-10 h-8 bg-transparent cursor-pointer block" 
+          <label
+              id="burger-container"
+              className="relative w-10 h-8 bg-transparent cursor-pointer block " 
               htmlFor="burger">
               <input 
                   type="checkbox" 
                   id="burger"
-                  className="burger hidden" />
-              <span className="block absolute w-full h-1 bg-white rounded-lg opacity-100 left-0 top-0 origin-[left, center]"></span>
-              <span className="block absolute w-full h-1 bg-white rounded-lg opacity-100 left-0 top-1/2 origin-[left, center] -translate-y-1/2"></span>
-              <span className="block absolute w-full h-1 bg-white rounded-lg opacity-100 left-0 top-full origin-[left, center] -translate-y-full"></span>
+                  className="hidden" />
+              <span className="burger-span bg-white block absolute w-full h-1 rounded-lg opacity-100 left-0 top-0 origin-[left, center]"></span>
+              <span className="burger-span bg-white block absolute w-full h-1 rounded-lg opacity-100 left-0 top-1/2 origin-[left, center] -translate-y-1/2"></span>
+              <span className="burger-span bg-white block absolute w-full h-1 rounded-lg opacity-100 left-0 top-full origin-[left, center] -translate-y-full"></span>
           </label>
         </MenuHandler>
         <MenuList className="mt-1">
