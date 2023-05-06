9754cc7218a387c2af367876cdd4131da8094eae
Miguel Ángel Medina Ramírez <migueldevinfo@gmail.com>
2023-05-06 16:00:37 +0200

    Style update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { Context } from "../context/Context";
 import { BiUser } from "react-icons/bi";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { auth } from "../config/firebase";
-import { FiLogIn } from "react-icons/fi"
+import { FiLogIn } from "react-icons/fi";
 import { AiOutlineForm } from "react-icons/ai";
 import { sendOrders } from "../controllers/orders/orders.functions";
 import { logOut } from "../controllers/register-login/functions";
@@ -22,9 +22,11 @@ function Navbar() {
   };
 
   return (
-    <div className="navbar flex rounded-3xl border-2 border-neutral-500 p-4 px-4">
+    <div className="navbar flex rounded-3xl border-2 border-neutral-400 p-4 px-4">
       <div className="flex-1">
-        <a className="text-4xl normal-case">METEC</a>
+        <a className="text-5xl tracking-tighter font-semibold relative before:content-['METEC'] before:bg-gradient-to-br before:from-[#00DBDE] before:to-[#FC00FF] before:absolute before:inset-0 before:bg-clip-text before:text-transparent before:animate-text-wave">
+          <h1>METEC</h1>
+        </a>
       </div>
       <div className="flex-none">
         <div className="dropdown-end dropdown">
@@ -48,7 +50,12 @@ function Navbar() {
               <ShoppingCart />
               <span className="text-info">Subtotal: ${totalPrice}</span>
               <div className="card-actions">
-                <button className="btn-primary btn-block btn" onClick={() => handleSendOrders()}>Checkout</button>
+                <button
+                  className="btn-primary btn-block btn"
+                  onClick={() => handleSendOrders()}
+                >
+                  Checkout
+                </button>
               </div>
             </div>
           </div>
@@ -60,11 +67,11 @@ function Navbar() {
                 tabIndex={0}
                 className="btn-ghost btn-circle avatar btn border-neutral-500 transition-all hover:border-transparent"
               >
-                <div className="w-10 rounded-full p-3">
+                <div className="w-10 rounded-full">
                   <img
-                    src={"./src/img/logo-metec.png"}
+                    src={"./src/img/user-image.webp"}
                     alt="avatar"
-                    className="rounded-full"
+                    className="rounded-full scale-125"
                   />
                 </div>
               </label>
@@ -101,20 +108,15 @@ function Navbar() {
                 className="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
               >
                 <li className="hover:text-edit">
-                  <a
-                    href="/login"
-                    className="justify-between"
-                  >
+                  <a href="/login" className="justify-between">
                     Sign in
-                    <FiLogIn className="text-white scale-150"/>
+                    <FiLogIn className="scale-150 text-white" />
                   </a>
                 </li>
                 <li className="hover:text-info">
-                  <a 
-                    href="/login"
-                    className="justify-between">
-                      Sign up
-                      <AiOutlineForm className="text-white scale-150"/>
+                  <a href="/login" className="justify-between">
+                    Sign up
+                    <AiOutlineForm className="scale-150 text-white" />
                   </a>
                 </li>
               </ul>
