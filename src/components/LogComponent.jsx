e2a84a65f7c16e9d0f52395638c6913632ba1eb3
Miguel Ángel Medina Ramírez <migueldevinfo@gmail.com>
2023-05-12 16:00:58 +0200

    Style update

--- src/components/LogComponent.jsx
+++ src/components/LogComponent.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function LogComponent({url}) {
   return (
-    <div className='absolute top-0 w-[170px] laptop:w-[400px] z-10 laptop:top-[22%] laptop:right-[5%] desktop:top-[28%] desktop:right-[10%] animate-shake-y'>
+    <a href='/' className='absolute top-0 w-[170px] laptop:w-[400px] z-10 laptop:top-[22%] laptop:right-[5%] desktop:top-[28%] desktop:right-[10%] animate-shake-y'>
         <img src={url} alt="Animation-Img" />
-    </div>
+    </a>
   )
 }
 
